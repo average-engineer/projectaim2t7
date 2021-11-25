3628a7c544ec87735934f14d68c08d1cd7831015
raj <raj.khamkar@rwth-aachen.de>
2021-11-25 13:01:05 +0100

    until resampling done!

--- preprocess.py
+++ preprocess.py
@@ -52,7 +52,7 @@ for path in paths:
     splitexp = re.split('_',experiment_name)
     subj=splitexp[0]
     gait=splitexp[1]
-    if not re.search(r'red', gait) and re.search(r'216',subj):
+    if not re.search(r'red', gait) and re.search(r'216',subj) and re.search(r'downstairs01',gait):
         # re.search(r'215',subj) or re.search(r'217',subj)
         gyr_file = glob.glob(path+'/Gyroscope.csv')
         acc_file = glob.glob(path+'/Accelerometer.csv')
@@ -66,11 +66,15 @@ for path in paths:
         sampling_frequency_gyr = np.round(len(time_gyr)/(time_gyr[-1]))
         samp_freq_acc = np.round(len(time_acc)/(time_acc[-1]))
         sampling_frequency_acc = np.round(len(time_acc)/(time_acc[-1]))
-        # print('Duration of Acc_exp:', time_acc[-1], 'Duration of Gyr_exp:', time_gyr[-1])
+    
+    #Filtering
         filter_acc =filter_data(acc,fs=sampling_frequency_acc,fc=2)
         filter_gyr = filter_data(gyr, fs=sampling_frequency_gyr, fc=4)
+    
+    #Cutting
         acc_cut, gyr_cut = cut_data(filter_acc,filter_gyr,samp_freq_acc)
-        # print(gyr_cut)
+    
+    #PCA
         data_gyr = scale(gyr_cut)
         pca_gyr = decomposition.PCA(n_components=1)
         pca_gyr.fit(data_gyr)
@@ -81,28 +85,25 @@ for path in paths:
         pca_acc.fit(data_acc)
         trans_acc = pca_acc.transform(data_acc)
         trans_df_acc = pd.DataFrame(trans_acc)
+    
+    #Segmenting
         peaks, diffpeaks = seg_data_acc(trans_df_acc.iloc[1:,0].values.astype(float))
         cycle_list_acc = []
         j=0
         for i in peaks:
             cycle_list_acc.append(trans_df_acc.iloc[j:i+1,0].values.astype(float))
             j=i+1
-        data_cycle_1 = pd.DataFrame()
-        data_cycle_2 = pd.DataFrame()
-        for i in range(len(cycle_list_acc)):
-            data_cycle_1[i] = cycle_list_acc
-            for j in data_cycle_1:
-                data_cycle_2 = data_cycle_1[j]
-print(data_cycle_2.iloc[0,:])
-        # plt.plot(data_cycle_2.iloc[0])
-        # plt.show()
-        # data_cycle_2.iloc[:].plot()
-        #     plt.plot(data_cycle_2)
-        #     plt.show()
-        # res_cyc = signal.resample(df_cyc[0],1750)
-        # plt.plot(res_cyc)
-        # # plt.plot(res_acc)
-        # # plt.plot(res_gyr)
-        # #  res_acc = signal.resample(trans_df_acc[0],1750)
-        # # res_gyr = signal.resample(trans_df_gyr[0],int(time_gyr[-1]))
-        # plt.show()
+        # print(cycle_list_acc)
+
+    #Resampling
+        df1 = pd.DataFrame(cycle_list_acc)
+        df2 = pd.DataFrame()
+        for i in range(df1.shape[1]):
+            for j in range(df1.shape[0]):
+                try :
+                    df2 = signal.resample(df1.iloc[0:j,0:i],50)
+                except :
+                    pass
+        print(df2.shape)
+        plt.plot(df2)
+        plt.show()
