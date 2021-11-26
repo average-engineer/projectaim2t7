3c7b298223d7bb3c22c05072c10a8ec104d2eecc
Ashutosh Mukherjee <ashutosh.mukherjeecpg@gmail.com>
2021-11-26 23:41:10 +0100

    Completed till data segmentation

---
+++ dataSegment.py
@@ -0,0 +1,34 @@
+#%% Function for segmenting the data
+
+from scipy import signal
+import numpy as np
+
+#%% Function Definition
+def dataSegment(acc,gyr):
+    peaksAcc, _ = signal.find_peaks(acc) # Returns indices of peaks in acceleration data
+    peaksGyr, _ = signal.find_peaks(gyr) # Returns indices of peaks in gyroscope data
+    
+# =============================================================================
+#     diff_peaksAcc = np.diff(peaksAcc)
+#     diff_peaksGyr = np.diff(peaksGyr)
+# =============================================================================
+    
+    # We return a list wherer each element of the list is the individual cycle/sample
+    # The data arrays are sliced into multiple cycles on the basis of peak indices obtained
+    
+    # Initialising lists
+    segAcc = []
+    segGyr = []
+    
+    jj = 0
+    for ii in peaksAcc:
+        cycAcc = acc[jj:ii + 1] # Cycle obtained 
+        segAcc.append(cycAcc)
+        
+        
+    jj = 0
+    for ii in peaksGyr:
+        cycGyr = gyr[jj:ii + 1] # Cycle obtained 
+        segGyr.append(cycGyr)
+
+    return segAcc,segGyr # Lists with each elements being the segmented cycles are returned
